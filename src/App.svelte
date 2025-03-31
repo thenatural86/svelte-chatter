2e3b42b779c0432194ccc7381405e8e6864b1962
thenatural86 <philip.smalls86@gmail.com>
2025-03-31 10:44:42 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,167 @@
+<script>
+  import Aside from './components/Aside.svelte'
+  import Header from './components/Header.svelte'
+  import TopWaves from './components/TopWaves.svelte'
+</script>
+
+<div id="app-container" class="app-container">
+  <TopWaves />
+  <Header />
+
+  <section>
+    <div class="container">
+      <main>
+        <div class="chit-entry">
+          <textarea placeholder="Say something..."></textarea>
+          <button class="btn-send fa-solid fa-location-arrow"></button>
+        </div>
+
+        <div class="chit">
+          <div class="author">Nauman</div>
+          <div class="handle">@recluze</div>
+          <div class="chit-content">
+            Does anyone here use an e-ink reader? Is it closer in feel (for the
+            eyes, not fingers) to an iPad or to an actual paper book? And this
+            can go on and on ...
+          </div>
+          <div class="chit-meta">
+            <div class="rechit"><i class="fa-solid fa-retweet"></i> 2</div>
+            <div class="like"><i class="fa-solid fa-thumbs-up"></i> 10</div>
+            <div class="save"><i class="fa-solid fa-share-alt"></i></div>
+            <div class="delete"><i class="fa-solid fa-trash"></i></div>
+          </div>
+        </div>
+
+        <div class="chit">
+          <div class="author">Albert Einstein</div>
+          <div class="handle">@alby</div>
+          <div class="chit-content">
+            My second awesome chit content goes here ... Looks horrible though!
+          </div>
+          <div class="chit-meta">
+            <div class="rechit"><i class="fa-solid fa-retweet"></i> 2</div>
+            <div class="like"><i class="fa-solid fa-thumbs-up"></i> 10</div>
+            <div class="save"><i class="fa-solid fa-share-alt"></i></div>
+            <div class="save"><i class="fa-solid fa-trash"></i></div>
+          </div>
+        </div>
+
+        <div class="chit">
+          <div class="author">Not Rich Harris</div>
+          <div class="handle">@notharris</div>
+          <div class="chit-content">
+            Create DApps (smart contracts using Ethereum blockchain) ... Best
+            seller course on sale right now for only $9.99 ... Don't miss out on
+            this great opportunity!
+          </div>
+          <div class="chit-meta">
+            <div class="rechit"><i class="fa-solid fa-retweet"></i> 2</div>
+            <div class="like"><i class="fa-solid fa-thumbs-up"></i> 10</div>
+            <div class="save"><i class="fa-solid fa-share-alt"></i></div>
+            <div class="save"><i class="fa-solid fa-trash"></i></div>
+          </div>
+        </div>
+      </main>
+      <Aside />
+    </div>
+  </section>
+</div>
+
+<style>
+  .container {
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+
+  section .container {
+    display: flex;
+    flex-direction: row;
+  }
+
+  section .container main {
+    flex-grow: 3;
+    flex-basis: 600px;
+    margin-left: 20px;
+  }
+
+  .chit {
+    border: 1px solid #62676d22;
+    display: flex;
+    flex-direction: column;
+    padding: 10px 20px;
+    font-weight: lighter;
+    background-color: #282c34;
+    box-shadow: 0px 0px 20px #1d2025;
+    margin-top: 30px;
+  }
+
+  .chit:hover {
+    border: 1px solid #62676d99;
+  }
+
+  .chit-meta {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 0px;
+    margin: 0px;
+    opacity: 0.5;
+    font-size: x-small;
+    border-top: 1px solid #dce4ec22;
+    padding-bottom: 0px;
+  }
+
+  .chit-meta i {
+    cursor: pointer;
+  }
+
+  .author {
+    font-weight: bold;
+    margin: 10px 0px;
+    margin-bottom: 0px;
+  }
+
+  .handle {
+    font-weight: 100;
+    opacity: 0.6;
+    font-size: 0.76em;
+  }
+
+  .chit-content {
+    margin: 20px 0px;
+    opacity: 0.8;
+    padding-right: 15px;
+    font-size: small;
+  }
+
+  .chit-entry {
+    border: 1px solid #62676d22;
+    display: flex;
+    flex-direction: row;
+    padding: 10px 20px;
+    font-weight: lighter;
+    background-color: #282c34;
+    box-shadow: 0px 0px 20px #1d2025;
+    margin-top: 30px;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .chit-entry textarea {
+    display: block;
+    flex-basis: 90%;
+    background-color: #282c34;
+    color: #dce4ec;
+    font-size: smaller;
+    padding: 10px;
+    border: 1px solid #dce4ec22;
+  }
+
+  .chit-entry .btn-send {
+    flex-basis: 20px;
+    text-align: right;
+    background-color: inherit;
+    border: none;
+    color: #dce4ec;
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,36 @@
+<!-- Header with information -->
+<nav>
+  <div class="container">
+    <span class="logo"><i class="fa-solid fa-location-arrow"></i>Chitter</span>
+  </div>
+</nav>
+
+<style>
+  .logo {
+    margin-left: 30px;
+    font-family: 'Source Sans Pro';
+    font-weight: 900;
+    opacity: 0.7;
+  }
+
+  .logo i {
+    margin-right: 10px;
+  }
+
+  nav .container {
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  nav .container ul {
+    display: flex;
+    height: 100%;
+  }
+
+  nav .container ul li {
+    margin: 10px;
+    list-style: none;
+  }
+</style>
